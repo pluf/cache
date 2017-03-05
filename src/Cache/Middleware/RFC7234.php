fd0d5fd0081dba9e4f05cc89fd4a3dd8f42c2d0d
hadi <hadi@dpq.co.ir>
2017-03-05 04:07:39 +0330

    RFC7234 middleware is implemented. Document is added.

--- src/Cache/Middleware/RFC7234.php
+++ src/Cache/Middleware/RFC7234.php
@@ -12,34 +12,54 @@ class Cache_Middleware_RFC7234
 {
 
     /**
-     * Adds some directives to header of resonse to cache response in client side.
+     * Adds some directives to header of resonse to cache response in client
+     * side.
      * This method is base on information from below url:
+     *
      * @see https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching
-     * 
-     * @param Pluf_HTTP_Request $request 
-     *  The request
-     * @param Pluf_HTTP_Response $resonse 
-     *  The response
+     *
+     * @param Pluf_HTTP_Request $request
+     *            The request
+     * @param Pluf_HTTP_Response $resonse
+     *            The response
      * @return Pluf_HTTP_Response The response
      */
     function process_response ($request, $response)
     {
         $view = $request->view;
-        $cacheable = false;
-        $revalidate = false;
-        $intermediate_cache = false;
-        $max_age = 604800; // 7 day
-        $etag = null;
-        if(array_key_exists('cacheable', $view)){
-            
-        }
+        $cacheable = array_key_exists('cacheable', $view) ? $view->cacheable : false;
+        $revalidate = array_key_exists('revalidate', $view) ? $view->revalidate : false;
+        $intermediate_cache = array_key_exists('revalidate', $view) ? $view->intermediate_cache : false;
+        // default max_age: 7 day
+        $max_age = array_key_exists('max_age', $view) ? $view->max_age : 604800;
+        $etag = method_exists($response, 'etag') ? $response->etag() : null;
         
-        $prefix = '/api/';
-        if (strncmp($request->query, $prefix, strlen($prefix)) === 0){
-            $response->headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
-            $response->headers['Pragma'] = 'no-cache';
-            $response->headers['Expires'] = '0';
+        //TODO: hadi, 1395: check if values in Cache-Controll should be separated by , or ;
+        
+        if (! $cacheable) {
+            $response->headers['Cache-Control'] = array_key_exists(
+                    'Cache-Control', $response->headers) ? $response->headers['Cache-Control'] .
+                     ', no-store' : 'no-store';
+            return $response;
         }
+        if($revalidate) {
+            $response->headers['Cache-Control'] = array_key_exists(
+                    'Cache-Control', $response->headers) ? $response->headers['Cache-Control'] .
+                     ', no-cache' : 'no-cache';
+        }
+        if($intermediate_cache){
+            $response->headers['Cache-Control'] = array_key_exists(
+                    'Cache-Control', $response->headers) ? $response->headers['Cache-Control'] .
+                    ', public' : 'public';
+        }else{
+            $response->headers['Cache-Control'] = array_key_exists(
+                    'Cache-Control', $response->headers) ? $response->headers['Cache-Control'] .
+                    ', private' : 'private';
+        }
+        $response->headers['Cache-Control'] = $response->headers['Cache-Control'] . ', max_age=' . $max_age;
+        if($etag !== null)
+            $response->headers['ETag'] = '"'.$etag.'"';
+        
         return $response;
     }
 }
